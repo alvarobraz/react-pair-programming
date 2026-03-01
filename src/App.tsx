caffb0c089ef178f8f72f78b9c52949d4f654e82
Álvaro Braz <alvarobraz83@gmail.com>
2026-03-01 18:59:57 -0300

    feat: create-project-with-vitest-and-add-initial-configurations

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,17 @@
+import Text from "@/components/ui/text";
+
 export default function App() {
 return (
-    <div className="text-pink-dark">Olá mundo!</div>
-  )
+    <div className="flex flex-col gap-2 ">
+      <Text variant="heading-lg" className="text-green-200">
+        Olá mundo!
+      </Text>
+      <Text className="heading-lg">Olá mundo!</Text>
+      <Text variant="body-md-regular">Olá mundo!</Text>
+      <Text variant="text-label">Olá mundo!</Text>
+      <Text variant="title-bold">Olá mundo!</Text>
+      <Text variant="sub-title">Olá mundo!</Text>
+      <Text>Levar o dog pra passear</Text>
+    </div>
+  );
 }
---
+++ src/components/ui/text.tsx
@@ -0,0 +1,42 @@
+import React from "react";
+import { tv, type VariantProps } from "tailwind-variants";
+
+// eslint-disable-next-line react-refresh/only-export-components
+export const textVariants = tv({
+  base: "font-sans text-white",
+  variants: {
+    variant: {
+      "heading-lg": "text-gray-100 text-xl leading-5 font-bold",
+      "body-md-regular": "text-gray-200 text-lg leading-5 font-regular",
+      "text-label": "text-gray-200 text-[0.625rem] leading-5 font-regular",
+      "title-bold": "text-gray-100 text-sm leading-[1.125rem] font-bold",
+      "sub-title": "text-gray-200 text-xs leading-4 font-regular",
+    },
+  },
+  defaultVariants: {
+    variant: "body-md-regular",
+  },
+});
+
+interface TextProps extends VariantProps<typeof textVariants> {
+  as?: keyof React.JSX.IntrinsicElements;
+  className?: string;
+  children?: React.ReactNode;
+}
+
+export default function Text({
+  as = "span",
+  variant,
+  className,
+  children,
+  ...props
+}: TextProps) {
+  return React.createElement(
+    as,
+    {
+      className: textVariants({ variant, className }),
+      ...props,
+    },
+    children
+  );
+}
